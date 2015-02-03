69b6f8de8d149f9176c31e84a69478ff4f88fef6
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-02-03 16:05:24 -0500

    add markdown support

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,5 @@
-Structure.create(description: "[1, 2, 3, 4, 5]")
-Structure.create(description: "{'McDermott, O'Reilly and Orn'=>'generate integrated niches',
+Structure.create(description: "```[1, 2, 3, 4, 5]```")
+Structure.create(description: "```{'McDermott, O'Reilly and Orn'=>'generate integrated niches',
  'Okuneva LLC'=>'transition ubiquitous channels',
  'Glover Group'=>'utilize interactive metrics',
  'Williamson, Stamm and Smitham'=>'incubate plug-and-play content',
@@ -8,13 +8,13 @@ Structure.create(description: "{'McDermott, O'Reilly and Orn'=>'generate integra
  'Schulist LLC'=>'envisioneer rich infrastructures',
  'Kutch-Krajcik'=>'maximize killer bandwidth',
  'Murphy Inc'=>'transform revolutionary models',
- 'Ondricka, Auer and Thiel'=>'embrace efficient platforms'}")
-Structure.create(description: "[-952, -482, 115, [63, -475, 378], -412, -25, -240, [-736, 5, -407]]")
+ 'Ondricka, Auer and Thiel'=>'embrace efficient platforms'}```")
+Structure.create(description: "```[-952, -482, 115, [63, -475, 378], -412, -25, -240, [-736, 5, -407]]```")
 
 5.times do
   array = Array.new
   10.times do
     array << rand(0..500)
   end
-  Structure.create(description: array.to_s)
+  Structure.create(description: "```" + array.to_s + "```")
 end
